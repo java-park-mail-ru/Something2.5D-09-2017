b8754ef114c1bc04bfb21ec53a91738d6f0da670
ZubAnt <ya.zubarevanton@yandex.ru>
2017-09-21 14:06:55 +0300

    rafactor somethings

--- src/main/java/com/tp/tanks/TanksApplication.java
+++ src/main/java/com/tp/tanks/TanksApplication.java
@@ -2,6 +2,8 @@ package com.tp.tanks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TanksApplication {
@@ -9,5 +11,10 @@ public class TanksApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(TanksApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
