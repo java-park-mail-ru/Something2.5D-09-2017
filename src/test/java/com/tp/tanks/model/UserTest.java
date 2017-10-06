42865b0aa4b4ff2b19fd70a47278c53e9110cf54
ZubAnt <ya.zubarevanton@yandex.ru>
2017-10-06 20:33:56 +0300

    connect to db by admin, add @ActiveProfiles("test") for test classes

--- src/test/java/com/tp/tanks/model/UserTest.java
+++ src/test/java/com/tp/tanks/model/UserTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
@@ -11,6 +12,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
 public class UserTest {
 
     User user;
--- src/test/java/com/tp/tanks/service/UserServiceTest.java
+++ src/test/java/com/tp/tanks/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
@@ -15,6 +16,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
 public class UserServiceTest {
 
     @Autowired
