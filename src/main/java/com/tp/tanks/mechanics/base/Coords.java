9bb6a0c3dc3ff91571fc920d7743e6a657421aaf
ZubAnt <ya.zubarevanton@yandex.ru>
2017-11-19 15:21:53 +0300

    add TankSnap.class into JsonSubTypes of Message

--- src/main/java/com/tp/tanks/mechanics/base/Coords.java
+++ src/main/java/com/tp/tanks/mechanics/base/Coords.java
@@ -1,4 +1,4 @@
-package com.tp.tanks.mechanic.base;
+package com.tp.tanks.mechanics.base;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jetbrains.annotations.NotNull;
--- src/main/java/com/tp/tanks/websocket/Message.java
+++ src/main/java/com/tp/tanks/websocket/Message.java
@@ -3,17 +3,17 @@ package com.tp.tanks.websocket;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-//import ru.mail.park.mechanics.base.TankSnap;
+import com.tp.tanks.mechanics.base.TankSnap;
 //import ru.mail.park.mechanics.base.ServerSnap;
 //import ru.mail.park.mechanics.requests.InitGame;
 //import ru.mail.park.mechanics.requests.JoinGame;
 
 @JsonTypeInfo(use= JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "class")
 @JsonSubTypes({
-        @Type(JoinGame.Request.class),
-        @Type(InitGame.Request.class),
-        @Type(ClientSnap.class),
-        @Type(ServerSnap.class),
+        @Type(TankSnap.class)
+//        @Type(JoinGame.Request.class),
+//        @Type(InitGame.Request.class),
+//        @Type(ServerSnap.class),
         })
 public abstract class Message {
 }
