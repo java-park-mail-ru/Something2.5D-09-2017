063e1d1a834ab68660ca98bc931c7ca405f80de8
ZubAnt <ya.zubarevanton@yandex.ru>
2017-12-02 22:01:05 +0300

    implemented counting processed snapshots in ServerSnapshotService

--- src/test/java/com/tp/tanks/game/GameMechanicsTest.java
+++ src/test/java/com/tp/tanks/game/GameMechanicsTest.java
@@ -1,32 +1,29 @@
 package com.tp.tanks.game;
 
 import com.tp.tanks.mechanics.GameMechanics;
+import com.tp.tanks.mechanics.GameMechanicsImpl;
+import com.tp.tanks.mechanics.MechanicsExecutor;
 import com.tp.tanks.mechanics.base.TankSnap;
-import com.tp.tanks.mechanics.internal.ServerSnapshotService;
 import com.tp.tanks.mechanics.internal.TankSnapshotService;
+import com.tp.tanks.mocks.MockServerSnapshotService;
 import com.tp.tanks.stubs.Generators;
 import com.tp.tanks.stubs.TankSnapFactory;
-import org.junit.Before;
-import org.junit.Test;
+import com.tp.tanks.websocket.RemotePointService;
+import org.jetbrains.annotations.NotNull;
+import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Matchers.anyListOf;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Matchers.any;
 
 
 @SuppressWarnings({"MagicNumber", "NullableProblems", "SpringJavaAutowiredMembersInspection"})
@@ -36,49 +33,78 @@ import static org.mockito.Mockito.verify;
 public class GameMechanicsTest {
 
     @Autowired
+    private TankSnapshotService tankSnapshotsService;
+
+    @NotNull
     private GameMechanics gameMechanics;
 
-    @MockBean
-    private ServerSnapshotService mockServerSnapshotService;
+    @Autowired
+    private MechanicsExecutor mechanicsExecutor;
+
+    @Autowired
+    private RemotePointService remotePointService;
+
+    private MockServerSnapshotService mockServerSnapshotService;
+
+//    @MockBean
+//    private ServerSnapshotService mockServerSnapshotService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GameMechanicsTest.class);
 
     @Captor
     private ArgumentCaptor<ArrayList<TankSnap>> captor;
 
+    private ArgumentCaptor<List<TankSnap>> argumentCaptor;
+
+
     @Before
-    public void setUp () {
-//        MockitoAnnotations.initMocks(this);
-    LOGGER.info("[GameMechanicsTest: setUp]");
-//        mockServerSnapshotService = mock(ServerSnapshotService.class);
+    public void before () {
 
+        mechanicsExecutor.lock();
+//        final Class<List<TankSnap>> cls = (Class<List<TankSnap>>)(Object)List.class;
+//        mockServerSnapshotService = mock(MockServerSnapshotService.class);
+//
+//        doAnswer((Answer<Object>) invocation -> {
+//            List<TankSnap> arg = invocation.getArgumentAt(0, cls);
+//            MockServerSnapshotService mock = (MockServerSnapshotService) invocation.getMock();
+//            mock.setProcessedSnapshots(arg.size());
+//            return null;
+//        }).when(mockServerSnapshotService).send(any());
+//
+//        when(mockServerSnapshotService.getNumbProcessedSnapshots()).thenAnswer((Answer<Integer>) invocation -> {
+//            MockServerSnapshotService mock = (MockServerSnapshotService) invocation.getMock();
+//            return mock.getProcessedSnapshots1();
+//        });
+//
+//        gameMechanics = new GameMechanicsImpl(tankSnapshotsService, mockServerSnapshotService);
 
-//        verify(mockServerSnapshotService).send(captor.capture());
+        mockServerSnapshotService = new MockServerSnapshotService(remotePointService);
+        gameMechanics = new GameMechanicsImpl(tankSnapshotsService, mockServerSnapshotService);
+    }
 
-
-//        doReturn("hello").when(mockServerSnapshotService).send(anyListOf(TankSnap.class));
+    @After
+    public void after() {
+        mechanicsExecutor.unlock();
     }
 
     @Test
-    public void tankSnapshotsServiceProcessSnapshotsTest() {
+    public void numbProcessedSnapshotsMustBeOne() {
         Long userId1 = Generators.generateLong();
-        Long userId2 = Generators.generateLong();
         int amount1 = 10;
-        int amount2 = 10;
-
         List<TankSnap> tankSnapsForUser1 = TankSnapFactory.createManyForUser(userId1, amount1);
-        List<TankSnap> tankSnapsForUser2 = TankSnapFactory.createManyForUser(userId2, amount2);
+
 
         for (TankSnap snap: tankSnapsForUser1) {
             gameMechanics.addTankSnapshot(userId1, snap);
         }
-        for (TankSnap snap: tankSnapsForUser2) {
-            gameMechanics.addTankSnapshot(userId2, snap);
-        }
-
 
         gameMechanics.gmStep(1000);
-//        List<TankSnap> test = captor.getValue();
-//        LOGGER.info("test size: " + test.size());
+        Assert.assertEquals(1,  mockServerSnapshotService.getNumbProcessedSnapshots());
+    }
+
+
+    public void numbProcessedSnapshotsMustBeZero() {
+        gameMechanics.gmStep(1000);
+        Assert.assertEquals(0,  mockServerSnapshotService.getNumbProcessedSnapshots());
     }
 }
