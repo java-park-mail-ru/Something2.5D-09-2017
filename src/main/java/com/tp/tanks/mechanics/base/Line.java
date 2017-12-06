810f49055c414634201bbd3c813c7b8d73ca7d26
pitikdmitry <PysaTN17>
2017-12-06 12:33:07 +0300

    add toLine method to tankSnap

--- src/main/java/com/tp/tanks/mechanics/base/Line.java
+++ src/main/java/com/tp/tanks/mechanics/base/Line.java
@@ -1,6 +1,6 @@
 package com.tp.tanks.mechanics.base;
 
-public class Line {
+class Line {
 
     private Long userId;
     private Coordinate startDot;
@@ -9,16 +9,55 @@ public class Line {
     private double koefK;
     private double koefB;
 
-    public Line(Long userId, Coordinate startDot, double turretAngle) {
+    Line(Long userId, Coordinate startDot, double turretAngle) {
         this.userId = userId;
         this.startDot = startDot;
         this.turretAngle = turretAngle;
-
-
+        estimateLine(startDot, turretAngle);
     }
 
     private void estimateLine(Coordinate startDot, double turretAngle) {
-        koefK = Math.tan(turretAngle);
-        koefB = startDot.getValY() - koefK * startDot.getValX();
+        this.koefK = Math.tan(turretAngle);
+        this.koefB = startDot.getValY() - koefK * startDot.getValX();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Coordinate getStartDot() {
+        return startDot;
+    }
+
+    public void setStartDot(Coordinate startDot) {
+        this.startDot = startDot;
+    }
+
+    public double getTurretAngle() {
+        return turretAngle;
+    }
+
+    public void setTurretAngle(double turretAngle) {
+        this.turretAngle = turretAngle;
+    }
+
+    public double getKoefK() {
+        return koefK;
+    }
+
+    public void setKoefK(double koefK) {
+        this.koefK = koefK;
+    }
+
+    public double getKoefB() {
+        return koefB;
+    }
+
+    public void setKoefB(double koefB) {
+        this.koefB = koefB;
     }
 }
