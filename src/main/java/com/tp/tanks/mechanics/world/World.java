73e8ebe32d159a8a51cb616cee6b1d4419371030
pitikdmitry <PysaTN17>
2017-12-08 17:53:41 +0300

    change logics to get new spawn point

--- src/main/java/com/tp/tanks/mechanics/world/World.java
+++ src/main/java/com/tp/tanks/mechanics/world/World.java
@@ -3,30 +3,37 @@ package com.tp.tanks.mechanics.world;
 import com.tp.tanks.mechanics.base.Coordinate;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
 
 
 public class World {
     private ArrayList<Box> boxes;
-    private ArrayList<Coordinate> spawnPoints;
+    private ArrayList<Coordinate> allSpawnPoints;
+    private ArrayList<Coordinate> currentSpawnPoints;
 
     public World() {
         this.boxes = Loader.loadBoxes();
-        this.spawnPoints = Loader.loadSpawnPoints();
+        this.allSpawnPoints = Loader.loadSpawnPoints();
+        this.currentSpawnPoints = new ArrayList<>(this.allSpawnPoints);
     }
 
     public ArrayList<Box> getBoxes() {
         return boxes;
     }
 
-    public ArrayList<Coordinate> getSpawnPoints() {
-        return spawnPoints;
+    public ArrayList<Coordinate> getAllSpawnPoints() {
+        return allSpawnPoints;
     }
 
     public Coordinate getTanksPosition() {
-        int numberOfPosition = ThreadLocalRandom.current().nextInt(0, spawnPoints.size());
-        Coordinate tankPosition = spawnPoints.get(numberOfPosition);
-        // spawnPoints.remove(tankPosition);
+        if(currentSpawnPoints.size() == 0) {
+            this.currentSpawnPoints = new ArrayList<>(this.allSpawnPoints);
+        }
+
+        int numberOfPosition = ThreadLocalRandom.current().nextInt(0, currentSpawnPoints.size());
+        Coordinate tankPosition = currentSpawnPoints.get(numberOfPosition);
+        currentSpawnPoints.remove(tankPosition);
         return tankPosition;
     }
 }
